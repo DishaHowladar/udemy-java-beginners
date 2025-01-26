3c99a137d40d8026e511cf05a37ea58bee068149
DishaHowladar <howladardisha@gmail.com>
2025-01-26 18:10:23 +0530

    first hello world commit

---
+++ firsteclips_project/src/com/disha/first/Helloworld.java
@@ -0,0 +1,10 @@
+package com.disha.first;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+
+	}
+
+}
---
+++ firsteclips_project/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module firsteclips_project {
+}
